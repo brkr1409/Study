680fdbc7f352794c10f8c055caa4112b1f5c34e8
brkr1409 <brkr1409@gmail.com>
2016-07-05 23:37:20 +0300

    testmassage

--- src/Animal.java
+++ src/Animal.java
@@ -2,7 +2,7 @@
  * Created by w0wfa on 05.07.2016.
  */
 public interface Animal {
-    void move();
+    int move();
     Animal reproduce();
     double getWeight();
     int getPosition();
--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,27 @@
 /**
  * Created by w0wfa on 05.07.2016.
  */
-public class Cat{
+public class Cat implements Animal{
+    int position=0;
+    int speed = 2;
+    double weight = 4;
+    @Override
+    public int move() {
+        return position+=speed;
+    }
+
+    @Override
+    public Animal reproduce() {
+        return new Cat();
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
 }
